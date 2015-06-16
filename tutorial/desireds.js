7f21eb3c429465566b43ee4d6e211ddab8ef02c2
kristiansl <kristianmeier@kristians-MacBook-Air.local>
2015-06-16 10:37:52 -0700

    Initial Commit

---
+++ tutorial/desireds.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+    chrome: {browserName: 'chrome', version: '42.0'},
+    firefox: {browserName: 'firefox', version: '36'},
+    explorer: {browserName: 'internet explorer', version: '11'},
+    android: {browserName: 'android', deviceName: 'Android Emulator', platformVersion: '4.4', platform: 'Linux'},
+ 	iphone: {browserName: 'iphone', platform: 'OS X 10.10', version:'8.2', deviceOrientation:'portrait'}
+
+
+};
